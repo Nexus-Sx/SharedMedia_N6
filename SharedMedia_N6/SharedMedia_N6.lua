0c9b618460ef2873d3ce11958f9be51c17d3f497
Nexus-Sx <50891691+Nexus-Sx@users.noreply.github.com>
2019-05-21 16:31:44 -0700

    Add files via upload

---
+++ SharedMedia_N6/SharedMedia_N6.lua
@@ -0,0 +1,39 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+ 
+-- -----
+--   BACKGROUND
+-- ----- 
+
+LSM:Register("background", "|cFF8C1616<N6>|r |cFFFFFF00Solid Background|r", [[Interface\Addons\SharedMedia_N6\background\Solid-Background.tga]])
+
+-- -----
+--   BORDER
+-- ----
+
+LSM:Register("border", "|cFF8C1616<N6>|r |cFFFFFF001px Border|r", [[Interface\Addons\SharedMedia_N6\border\One-Pixel-Border.tga]])
+
+-- -----
+--   FONT
+-- -----
+
+LSM:Register("font", "|cFF8C1616<N6>|r |cFFFFFF00Roboto Mono Bold|r", [[Interface\Addons\SharedMedia_N6\font\RobotoMono-Bold.ttf]])
+LSM:Register("font", "|cFF8C1616<N6>|r |cFFFFFF00Roboto Black|r", [[Interface\Addons\SharedMedia_N6\font\Roboto-Black.ttf]])
+
+-- -----
+--   SOUND
+-- -----
+
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Bait|r", [[Interface\Addons\SharedMedia_N6\sound\Bait.ogg]]) 
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Cancel|r", [[Interface\Addons\SharedMedia_N6\sound\Cancel.ogg]]) 
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Left|r", [[Interface\Addons\SharedMedia_N6\sound\Left.ogg]]) 
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Move|r", [[Interface\Addons\SharedMedia_N6\sound\Move.ogg]]) 
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Right|r", [[Interface\Addons\SharedMedia_N6\sound\Right.ogg]]) 
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Spread|r", [[Interface\Addons\SharedMedia_N6\sound\Spread.ogg]]) 
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Stack|r", [[Interface\Addons\SharedMedia_N6\sound\Stack.ogg]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
+
+LSM:Register("statusbar", "<N6> Flat Dark", [[Interface\Addons\SharedMedia_N6\statusbar\Flat-Dark.tga]])
+LSM:Register("statusbar", "<N6> Flat Light", [[Interface\Addons\SharedMedia_N6\statusbar\Flat-Light.tga]])
