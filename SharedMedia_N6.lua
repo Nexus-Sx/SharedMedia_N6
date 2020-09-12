2b6517a401c042b594fbf7fee71a175ff74810fc
Nexus-Sx <50891691+Nexus-Sx@users.noreply.github.com>
2020-09-12 11:10:58 -0700

    Update SharedMedia_N6.lua

--- SharedMedia_N6.lua
+++ SharedMedia_N6.lua
@@ -24,6 +24,24 @@ LSM:Register("font", "|cFF8C1616<N6>|r |cFFFFFF00Roboto Black|r", [[Interface\Ad
 -- -----
 
 LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Bait|r", [[Interface\Addons\SharedMedia_N6\sound\Bait.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 01|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 01.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 02|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 02.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 03|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 03.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 04|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 04.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 05|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 05.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 06|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 06.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 07|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 07.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 08|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 08.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 09|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 09.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 10|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 10.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 11|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 11.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 12|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 12.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 13|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 13.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 14|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 14.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 15|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 15.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 16|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 16.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 17|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 17.ogg]])
+LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00BotW 18|r", [[Interface\Addons\SharedMedia_N6\sound\BotW 18.ogg]])
 LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Cancel|r", [[Interface\Addons\SharedMedia_N6\sound\Cancel.ogg]])
 LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Combust|r", [[Interface\Addons\SharedMedia_N6\sound\Combust.ogg]])
 LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Cruise Scream Long|r", [[Interface\Addons\SharedMedia_N6\sound\Cruise Scream Long.ogg]])
@@ -49,5 +67,6 @@ LSM:Register("sound", "|cFF8C1616<N6>|r |cFFFFFF00Whistle|r", [[Interface\Addons
 --   STATUSBAR
 -- -----
 
-LSM:Register("statusbar", "<N6> Flat Dark", [[Interface\Addons\SharedMedia_N6\statusbar\Flat-Dark.tga]])
-LSM:Register("statusbar", "<N6> Flat Light", [[Interface\Addons\SharedMedia_N6\statusbar\Flat-Light.tga]])
+LSM:Register("statusbar", "|cFF8C1616<N6>|r |cFFFFFF00Flat Dark|r", [[Interface\Addons\SharedMedia_N6\statusbar\Flat-Dark.tga]])
+LSM:Register("statusbar", "|cFF8C1616<N6>|r |cFFFFFF00Flat Medium|r", [[Interface\Addons\SharedMedia_N6\statusbar\Flat-Medium.tga]])
+LSM:Register("statusbar", "|cFF8C1616<N6>|r |cFFFFFF00Flat Light|r", [[Interface\Addons\SharedMedia_N6\statusbar\Flat-Light.tga]])
